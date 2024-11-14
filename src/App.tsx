76e25fb0b28be06d6df7a2df94f2ba9d196f5f0a
Sean Thompson JR <65671096+SeanThompsonJr@users.noreply.github.com>
2024-11-14 09:45:03 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// setup video: https://youtu.be/SiMIBb20S7c?si=TkCcqnS05w6_7r0P
+import React from "react";
+import "./index.css";
+export default function app() {
+  return (
+    <>
+      <div className="bg-orange-400">t</div>
+    </>
+  );
+}
---
+++ webpack.renderer.config.ts
@@ -0,0 +1,31 @@
+import type { Configuration } from "webpack";
+
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
+import { postcss } from "tailwindcss";
+
+rules.push({
+  test: /\.css$/,
+  use: [
+    { loader: "style-loader" },
+    { loader: "css-loader" },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+      },
+    },
+  ],
+});
+
+export const rendererConfig: Configuration = {
+  module: {
+    rules,
+  },
+  plugins,
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+  },
+};
